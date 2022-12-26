65b0bd0c8cf53881021eed4f1267314edd5c4cbe
Lucélia Batista <lucelia.ti@gmail.com>
2022-12-26 10:35:56 -0300

    Number of players for the game to take place limited to 2, not less than 2

--- adicionarJogador.cs
+++ adicionarJogador.cs
@@ -5,34 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JogoDaVelha
-{
-    class adicionarJogador
-    {
+namespace JogoDaVelha {
+    class adicionarJogador {
         public List<string> names = new List<string>();
         public List<string> nickNames = new List<string>();
         public List<int> ages = new List<int>();
         public List<string> cpfs = new List<string>();
 
-        public void AdicionarJogador(List<string> names, List<string> nickNames, List<int> ages, List<string> cpfs)
-        {
-            int quantidade;
-            do
-            {
-                Console.WriteLine("Comece informando aqui abaixo a quantidade de jogadores deste jogo:");
-                quantidade = (int.Parse(Console.ReadLine()));
-
-                if (quantidade < 2)
-                {
-                    Console.WriteLine("Para o jogo ocorrer, é necessário haver mais de um jogador");
-                }
-
-            } while (quantidade < 2);
-
-
-
-            for (int i = 0; i < quantidade; ++i)
-            {
+        public void AdicionarJogador( List<string> names, List<string> nickNames, List<int> ages, List<string> cpfs ) {
+      
+            for (int i = 0; i < 2; ++i) {
                 Console.WriteLine($"Digite o nome do(a) {i + 1}º jogador: ");
                 names.Add(Console.ReadLine());
                 Console.WriteLine("Digite o apelido: ");
@@ -46,6 +28,10 @@ namespace JogoDaVelha
                 Console.WriteLine("Jogador inscrito!");
                 Console.WriteLine("-----------------------------------");
             }
+
+            do {
+                Console.WriteLine("Para o jogo ocorrer, é necessário haver 2 jogadores");
+            } while (names.Count < 2);
 
         }
 
